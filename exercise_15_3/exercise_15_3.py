ab50a3030d8d14916776667bd744a64855fd17af
Sander Hofstee <sander.hofstee@topicus.nl>
2022-05-24 21:20:36 +0200

    exercise 15.3

--- exercise_15_3/exercise_15_3.py
+++ exercise_15_3/exercise_15_3.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
+
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(num_points=1_000, distance_x=[0, 5, 10, 50], distance_y=[0, 100, 200])
     rw.fill_walk()
     # Plot the points in the walk.
     plt.style.use('classic')
@@ -13,13 +14,14 @@ while True:
     point_numbers = range(rw.num_points)
 
     # Plot the random walk
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,  edgecolor='none', s=1)
+    # ax.plot(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    ax.plot(rw.x_values, rw.y_values)
 
     # Emphasize the first and last points.
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
-               s=100)
-
+    # ax.plot(0, 0, c='green', edgecolors='none', s=100)
+    ax.plot(0, 0, c='green')
+    # ax.plot(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax.plot(rw.x_values[-1], rw.y_values[-1], c='red')
     # Remove the axes.
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
--- exercise_15_3/random_walk.py
+++ exercise_15_3/random_walk.py
@@ -1,27 +1,36 @@
 from random import choice
 
 
+def get_step(distance_options):
+    direction = choice([1, -1])
+    distance = choice(distance_options)
+    return direction * distance
+
+
 class RandomWalk:
 
-    def __init__(self, num_points=5000):
+    def __init__(self, num_points=5000, distance_x=None, distance_y=None):
         """Initialize attributes of a walk."""
+        if distance_x is None:
+            distance_x = [0, 1, 2, 3, 4]
+        if distance_y is None:
+            distance_y = [0, 1, 2, 3, 4]
         self.num_points = num_points
         # All walks start at (0, 0).
         self.x_values = [0]
         self.y_values = [0]
+        self.distance_x = distance_x
+        self.distance_y = distance_y
 
     def fill_walk(self):
         """Calculate all the points in the walk."""
         # Keep taking steps until the walk reaches the desired length.
         while len(self.x_values) < self.num_points:
             # Decide which direction to go and how far to go in that direction.
-            x_direction = choice([1, -1])
 
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = get_step(self.distance_x)
+            y_step = get_step(self.distance_y)
+
             # Reject moves that go nowhere.
             if x_step == 0 and y_step == 0:
                 continue
